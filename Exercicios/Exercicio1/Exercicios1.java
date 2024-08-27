123eef2fc76eb6e0dca2c0ff6554dab0d64cac36
CarineCadorin <carinecadorin15@gmail.com>
2024-08-27 12:21:51 -0300

    Exercicio 1 que foi passado em aula

--- Exercicios/Exercicio1/Exercicios1.java
+++ Exercicios/Exercicio1/Exercicios1.java
@@ -1,8 +1,32 @@
 package Exercicio1;
-
+import java.util.Scanner;
 public class Exercicios1 {
-
     public static void main(String[] args) {
-        System.out.println("asdasdasd");
+
+        Scanner leitor = new Scanner(System.in);
+
+        System.out.println("Qual ambiente você prefere?");
+        System.out.println("1 - Sci-fi");
+        System.out.println("2 - Medieval");
+        int opcaoAmbientacao = leitor.nextInt();
+
+        System.out.println("Qual gênero você prefere?");
+        System.out.println("1 - Comédia");
+        System.out.println("2 - Drama");
+        int opcaoGenero = leitor.nextInt();
+
+        if (opcaoAmbientacao == 1 && opcaoGenero == 1) {
+            System.out.println("Homens de preto");
+        } else if (opcaoAmbientacao == 1 && opcaoGenero == 2) {
+            System.out.println("Arrival");
+        } else if (opcaoAmbientacao == 2 && opcaoGenero == 1) {
+            System.out.println("Shrek");
+        } else if (opcaoAmbientacao == 2 && opcaoGenero == 2) {
+            System.out.println("Gladiador");
+        } else {
+            System.out.println("Opção inválida");
+        }
+
+        leitor.close();
     }
 }
